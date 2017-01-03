aecd3c2bb2a9d0a5bf995c67fbafbf83ae37bfe6
vsoftic <vsoftic@ranorex.com>
2017-01-03 14:48:42 +0100

    added props example

--- demo.js
+++ demo.js
@@ -14,24 +14,32 @@ const renderTable = (data) => {
   );
 };
 
-let jsx = <div>
+let jsx = (props) => <div>
   <h2>Some JSX generated content.</h2>
+  <p>{props.text}</p>
 </div>
 
-let content = () => div({"id": "main-content"},
-  h2({"class": ["red", "blue"]}, "Hello World"),
+let content = (props) => div({"id": props.mountId},
+  h2({"class": ["red", "blue"]}, props.greetingHeader),
   p(null, "Lorem ipsum dolor sit amet"),
   br(),
-  p({"style": {"textDecoration": "underline"}}, "Das ist ein Test"),
-  renderTable( [ 
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-  ]),
+  p({"style": props.styles}, "Das ist ein Test"),
+  renderTable(props.tableData),
   p({}, true),
   p(null, Math.random(0, 1)),
   p(null, null),
-  jsx
+  jsx({
+    "text": "Jaddaa jaddaa jadaa"
+  })
 );
 
-VLibRender(content(), "body");
+VLibRender(content({
+  "mountId": "main-content",
+  "greetingHeader": "Hello World !!!",
+  "styles": {"textDecoration": "underline"},
+  "tableData": [ 
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]
+}), "body");
