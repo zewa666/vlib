e10a3e6ad3a3e5798f111f0819b4ca492a20f0a6
Vildan Softic <zewa666@gmail.com>
2017-01-10 18:27:37 +0100

    linter fixes

--- webpack.tests.js
+++ webpack.tests.js
@@ -1,5 +1,8 @@
+/* global require */
+
 import "babel-polyfill";
 
 // this regex matches all *.spec.js files in the folders src and lib
-var context = require.context('.', true, /(src|lib).+\.spec.js$/);
+const context = require.context(".", true, /(src|lib).+\.spec.js$/);
+
 context.keys().forEach(context);
